4359593c4b365e77fec84e6db0fa72d3ef94f5f0
alexkasp <salihov1983@gmail.com>
2017-06-15 18:40:31 +0300

    add read siplog

--- extregmon/testCommand.cpp
+++ extregmon/testCommand.cpp
@@ -1,5 +1,6 @@
 #include "testCommand.h"
 #include <iostream>
+#include <fstream>
 
 
 testCommand::testCommand()
@@ -44,5 +45,106 @@ int testCommand::checkLineStatus(string login, boost::property_tree::ptree& data
 	string msg = "extregTest:";
 	msg+=" external::18136325029             sip:410997@sip.zadarma.com        REGED     0.00  0/1     1/1";
 	data.add("checkLineStatus", msg);
+	return 0;
+}
+
+
+// scan log file for log with this line
+int testCommand::getLineLog(string login, boost::property_tree::ptree& pt)
+{
+	ifstream log("/var/log/freeswitch/freeswitch.log");
+	if (log)
+	{
+		char data[10000];
+		std::streamoff ptr;
+		ptr = log.tellg();
+		while (log.getline(data, 8096))
+		{
+			if (strstr(data, login.c_str()))
+			{
+			//	if ((data[3] == '-')&& (data[4] == '-') && (data[5] == '-') && (data[6] == '-') && (data[7] == '-') && (data[8] == '-') && (data[9] == '-') )
+				//	continue;
+				int sendcounter = 0;
+				if (SetPositionToBeginSipHeader(log,sendcounter))
+					SendSipPacket(log,sendcounter,pt);
+				else
+				{
+					break;
+				}
+			}
+		}
+		log.close();
+		log.clear();
+
+		
+	}
+	return 0;
+}
+
+
+// set position one line back
+int testCommand::LineBackLog(std::ifstream& log)
+{
+	log.seekg(-2, log.cur);
+	while (true)
+	{
+		log.seekg(-2, log.cur);
+		auto ptr = log.tellg();
+		if (ptr < 0) return 0;
+		char in = log.get();
+		if (in == '\n')
+			return 1;
+	}
+
+	return 0;
+}
+
+
+// set read ptr to start sip packet
+int testCommand::SetPositionToBeginSipHeader(std::ifstream& log, int&sendcounter)
+{
+	
+	char data[8096];
+	while (true)
+	{
+		LineBackLog(log);
+		LineBackLog(log);
+		std::cout << "SetPosition" << std::endl;
+		log.getline(data, 8096);
+		if (strstr(data, STARTPACKETSIGNATURE1.c_str()) || 
+			strstr(data, STARTPACKETSIGNATURE2.c_str()) || 
+			strstr(data, STARTPACKETSIGNATURE3.c_str()) || strstr(data, STARTPACKETSIGNATURE4.c_str())|| 
+			((data[3] == '-') && (data[4] == '-') && (data[5] == '-') && (data[6] == '-') && (data[7] == '-') && (data[8] == '-') && (data[9] == '-')))
+		{
+			LineBackLog(log);
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+
+int testCommand::SendSipPacket(std::ifstream& log, int sendcounter, boost::property_tree::ptree& pt)
+{
+	char packetdata[8096];
+	char data[8096];
+	log.getline(packetdata, 8096);
+	pt.put_value(packetdata);
+	while (true)
+	{
+		--sendcounter;
+		log.getline(data, 8096);
+		pt.put(packetdata, data);
+		if (strstr(data, STARTPACKETSIGNATURE1.c_str()) ||
+			strstr(data, STARTPACKETSIGNATURE2.c_str()) ||
+			strstr(data, STARTPACKETSIGNATURE3.c_str()) || strstr(data, STARTPACKETSIGNATURE4.c_str()) ||
+			((data[3] == '-') && (data[4] == '-') && (data[5] == '-') && (data[6] == '-') && (data[7] == '-') && (data[8] == '-') && (data[9] == '-')))
+		{
+			
+			return 1;
+		}
+	}
+
 	return 0;
 }
--- extregmon/testCommand.h
+++ extregmon/testCommand.h
@@ -3,6 +3,10 @@
 class testCommand :
 	public ICommand
 {
+	string STARTPACKETSIGNATURE1 = "bytes to udp/";
+	string STARTPACKETSIGNATURE2 = "bytes from udp/";
+	string STARTPACKETSIGNATURE3 = "bytes to tcp/";
+	string STARTPACKETSIGNATURE4 = "bytes from tcp/";
 public:
 	testCommand();
 	virtual ~testCommand();
@@ -10,5 +14,12 @@ public:
 	int RunParse(boost::property_tree::ptree& data);
 	// check if line exists on server and find out status of registration
 	int checkLineStatus(string login, boost::property_tree::ptree& data);
+	// scan log file for log with this line
+	int getLineLog(string login, boost::property_tree::ptree& pt);
+	// set position one line back
+	int LineBackLog(std::ifstream& log);
+	// set read ptr to start sip packet
+	int SetPositionToBeginSipHeader(std::ifstream& log, int&sendcounter);
+	int SendSipPacket(std::ifstream& log, int sendcounter, boost::property_tree::ptree& pt);
 };
 
